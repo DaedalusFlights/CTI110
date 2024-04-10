bf9a45036e5e506a757b54f69f19480ad879834e
DaedalusFlights <166663365+DaedalusFlights@users.noreply.github.com>
2024-04-10 18:47:09 -0400

    Add files via upload

---
+++ p1hw1_LewisEthan.py
@@ -0,0 +1,16 @@
+ # Ethan Lewis
+ # 4/10/2024
+ # P1HW1
+ # Basic output with variables
+
+print('Enter integer:', end=' ')
+user_num = int(input())
+print(user_num)
+print(user_num * user_num)
+print(user_num * user_num * user_num)
+
+print('Enter another integer:', end=' ')
+user_num2 = int(input())
+print(user_num2)
+print(user_num, '+', user_num2, 'is', user_num + user_num2)
+print(user_num, '*', user_num2, 'is', user_num * user_num2)
