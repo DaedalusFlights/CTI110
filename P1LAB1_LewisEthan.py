844419698dfc94a7d9bedf7d03f03b98cef1b323
DaedalusFlights <166663365+DaedalusFlights@users.noreply.github.com>
2024-04-10 17:56:01 -0400

    Add files via upload

---
+++ P1LAB1_LewisEthan.py
@@ -0,0 +1,13 @@
+ # Ethan Lewis
+ # 4/10/2024
+ # Assignment Name
+ # This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+print('Enter your first name:', end=' ')
+firstname = input()
+
+print('Enter your last name:', end=' ')
+lastname = input()
+
+
+print('Hello,', firstname, lastname,'! Welcome to CTI-110')
